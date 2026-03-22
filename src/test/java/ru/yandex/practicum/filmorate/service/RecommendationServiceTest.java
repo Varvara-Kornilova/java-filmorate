b5edfd3c7de83c054328e0e2dc0f8004b8d70299
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-22 22:10:54 +0300

    Update RecommendationServiceTest.java

--- src/test/java/ru/yandex/practicum/filmorate/service/RecommendationServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/RecommendationServiceTest.java
@@ -13,7 +13,6 @@ import ru.yandex.practicum.filmorate.service.recommendation.RecommendationServic
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
