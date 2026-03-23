34587794f70f9eb9ae0e0fc7d988de5de9f3ceeb
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-24 00:53:24 +0300

    Update UserDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
@@ -43,7 +43,7 @@ public class UserDbStorage extends BaseDbStorage<User> implements UserStorage {
                 SELECT film_id FROM likes WHERE user_id = ?
             ),
             other_users_overlap AS (
-                SELECT 
+                SELECT
                     l.user_id AS other_user_id,
                     COUNT(l.film_id) AS overlap_count
                 FROM likes l
